31470416afb7488c759d92aef26d3b1309bdbba6
matiss <matissjurevics@gmail.com>
2022-05-07 11:02:51 +0100

    completed basic ui

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+<main class="h-screen w-screen bg-slate-700">
+    <nav class="py-12 w-full flex flex-row justify-center items-center">
+        <h1 class="p-2 text-6xl font-semibold text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-pink-500">Moodgetter</h1>
+    </nav>
+    <section class="flex flex-col items-center">
+        <button class="py-3 px-8 mb-16 w-40 bg-gradient-to-br from-purple-500 to-pink-500 rounded-xl text-xl font-semibold text-white transition ease-in-out duration-300 hover:-translate-y-1 hover:shadow-md hover:shadow-purple-500/50">Get Mood</button>
+        <form class="flex flex-col " action="">
+            <input type="text" placeholder="Your mood" class="text-center outline-none border-b-2 border-white/25 bg-transparent text-slate-300 transition ease-in-out hover:scale-125 hover:border-white/50 focus:border-white/50 focus:scale-125">
+            <button class="py-3 px-8 mt-16 mx-auto w-40 bg-gradient-to-tr from-orange-500 to-pink-500 rounded-xl text-xl font-semibold text-white transition ease-in-out duration-300 hover:-translate-y-1 hover:shadow-md hover:shadow-pink-500/50">Get Mood</button>
+
+        </form>
+    </section>
+</main>
